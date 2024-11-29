1d552564c39dedbccf1f1e304ca9d54608f8bd52
Nyan Cat <theyaemoribot@gmail.com>
2024-11-29 09:02:50 -0500

    𝙔𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Game•kiss.js
+++ plugins/Game•kiss.js
@@ -3,16 +3,20 @@ import fetch from 'node-fetch'
 let MessageType = (await import('@whiskeysockets/baileys')).default
 let handler = async (m, { conn}) => {
 try {   
-if (!m.sender) return m.reply(`🚩 Etiqueta o menciona a alguien`)
+let who
+if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
+else who = m.chat
+if (!who) return m.reply(`🚩 Etiqueta o menciona a alguien`)
 if(m.quoted?.sender) m.mentionedJid.push(m.quoted.sender)
 if(!m.mentionedJid.length) m.mentionedJid.push(m.sender)
 let res = await fetch('https://nekos.life/api/kiss')
 let json = await res.json()
 let name = conn.getName(m.sender)
-let name2 = conn.getName(m.sender)
+let name2 = conn.getName(who)
+if (m.isGroup){
 let { url } = json
 let stiker = await sticker(null, url, `${name} le dio besos a ${name2}`)
-conn.sendFile(m.chat, stiker, null, { asSticker: true })
+conn.sendFile(m.chat, stiker, null, { asSticker: true })}
 } catch (e) { }}
 handler.help = ['kiss']
 handler.tags = ['fun']
