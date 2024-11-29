4f44b04fd44b52562d258a05f821dd27c783d026
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2024-11-29 08:41:51 -0500

    𝙔𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Game•alimentar.js
+++ plugins/Game•alimentar.js
@@ -5,13 +5,13 @@ let handler = async (m, { conn}) => {
 try {
 if(m.quoted?.sender) m.mentionedJid.push(m.quoted.sender)
 if(!m.mentionedJid.length) m.mentionedJid.push(m.sender)
-let res = await fetch('https://api.waifu.pics/sfw/pat')
+let res = await fetch('https://nekos.life/api/v2/img/feed')
 let json = await res.json()
 let { url } = json
-let stiker = await sticker(null, url, `+${m.sender.split('@')[0]} le dio palmaditas a ${m.mentionedJid.map((user)=>(user === m.sender)? 'alguien ': `+${user.split('@')[0]}`).join(', ')}`)
+let stiker = await sticker(null, url, `+${m.sender.split('@')[0]} está alimentando a ${m.mentionedJid.map((user)=>(user === m.sender)? 'alguien ': `+${user.split('@')[0]}`).join(', ')}`)
 conn.sendFile(m.chat, stiker, null, { asSticker: true })
 } catch (e) { }}
-handler.help = ['pat']
+handler.help = ['alimentar']
 handler.tags = ['fun']
-handler.command = ['pat', 'palmaditas', 'cariños', 'mimos', 'patt']
+handler.command = ['alimentar', 'food']
 export default handler
