bf318853dfd1c8e8d0f1cd9e08cbe5ac3bf077cc
YaemoriBot-MD <theyaemoribot@gmail.com>
2024-12-01 17:43:56 -0500

    𝙔𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Owner•getcontact.js
+++ plugins/Owner•getcontact.js
@@ -5,17 +5,6 @@ let handler = async (m, { conn, args }) => {
         await m.reply('Por favor, menciona el número que quieres que guarde.')
         return
     }
-
-    // Pedir el nombre con el que se desea guardar el contacto
-    let response = await m.reply('Por favor, proporcione el nombre con el que desea guardar el contacto.')
-
-    // Esperar la respuesta del usuario
-    conn.on('chat-update', async chatUpdate => {
-        if (chatUpdate.messages && chatUpdate.count) {
-            let message = chatUpdate.messages.all()[0]
-            if (message.key.remoteJid === m.chat && !message.key.fromMe && message.message.conversation) {
-                let username = message.message.conversation
-                // Enviar el contacto con el nombre proporcionado
                 await conn.sendContact(m.chat, [[`${who.split`@`[0]}@s.whatsapp.net`, `${username}`]], m)
             }
         }
