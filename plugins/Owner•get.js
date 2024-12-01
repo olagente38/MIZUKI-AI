a848c0124c3e43155232cd65665f422808701eac
YaemoriBot-MD <theyaemoribot@gmail.com>
2024-12-01 17:28:28 -0500

    𝙔𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Owner•get.js
+++ plugins/Owner•get.js
@@ -1,26 +1,26 @@
-import fetch from 'node-fetch';
-import {format} from 'util';
-const handler = async (m, {text}) => {
-    if (!/^https?:\/\//.test(text)) return m.reply('🌵 Te faltó el *url* de la pagina.');
-  const _url = new URL(text);
-  const url = global.API(_url.origin, _url.pathname, Object.fromEntries(_url.searchParams.entries()), 'APIKEY');
-  const res = await fetch(url);
-  if (res.headers.get('content-length') > 100 * 1024 * 1024 * 1024) {
-    // delete res
-    throw `Content-Length: ${res.headers.get('content-length')}`;
-  }
-  if (!/text|json/.test(res.headers.get('content-type'))) return conn.sendFile(m.chat, url, 'file', text, m);
-  let txt = await res.buffer();
-  try {
-    txt = format(JSON.parse(txt + ''));
-  } catch (e) {
-    txt = txt + '';
-  } finally {
-    m.reply(txt.slice(0, 65536) + '');
-  }
-};
-handler.help = ['fetch', 'get'];
-handler.tags = ['owner'];
-handler.command = ['fetch', 'get'];
-handler.rowner = true;
-export default handler;
+import fetch from 'node-fetch'
+import { format } from 'util'
+let handler = async (m, { text, usedPrefix, command }) => {
+    if (!/^https?:\/\//.test(text)) return conn.reply(m.chat, `🍄 Te faltó el *link* de la pagina\n\n✨️Ejemplo:\n> » ${usedPrefix + command} https://github.com`, m, rcanal)
+    let _url = new URL(text)
+    let url = global.API(_url.origin, _url.pathname, Object.fromEntries(_url.searchParams.entries()), 'APIKEY')
+    let res = await fetch(url)
+    if (res.headers.get('content-length') > 100 * 1024 * 1024 * 1024) {
+        // delete res
+        throw `Content-Length: ${res.headers.get('content-length')}`
+    }
+    if (!/text|json/.test(res.headers.get('content-type'))) return conn.sendFile(m.chat, url, 'file', text, m)
+    let txt = await res.buffer()
+    try {
+        txt = format(JSON.parse(txt + ''))
+    } catch (e) {
+        txt = txt + ''
+    } finally {
+        m.reply(txt.slice(0, 65536) + '')
+    }
+}
+handler.help = ['get']
+handler.tags = ['owner']
+handler.command = ['get', 'getweb']
+
+export default handler
