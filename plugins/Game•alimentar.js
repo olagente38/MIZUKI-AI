5eff83d062ef44bd150750d619470a30c188c7be
꧁☆*κɪɴɢ*☆꧂ <argumedodie@gmail.com>
2024-12-01 19:20:16 -0500

    𝙔𝙚𝙢𝙤𝙧𝙞𝘽𝙤𝙩-𝙈𝘿 ~ 𝘿𝙚𝙫𝘿𝙞𝙚𝙜𝙤

--- plugins/Game•alimentar.js
+++ plugins/Game•alimentar.js
@@ -1,85 +1,25 @@
-import fs from 'fs'
-import qrcode from 'qrcode'
-
-let subBotCodes = {} // Objeto para almacenar los códigos generados y su estado
-
-// Generar un código de 8 dígitos
-function generateSubBotCode() {
-    return Math.floor(10000000 + Math.random() * 90000000).toString()
-}
-
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    let option = args[0]
-
-    if (!option) {
-        await m.reply(`Por favor, proporciona una opción válida: *${usedPrefix}${command} [codigo|qr]*`)
-        return
-    }
-
-    if (option.toLowerCase() === 'codigo') {
-        // Generar un código de 8 dígitos
-        let code = generateSubBotCode()
-        subBotCodes[code] = true // Marcar el código como válido y no utilizado
-
-        await m.reply(`Tu código de vinculación es: ${code}`)
-    } else if (option.toLowerCase() === 'qr') {
-        // Generar un código QR
-        let code = generateSubBotCode()
-        subBotCodes[code] = true // Marcar el código como válido y no utilizado
-
-        qrcode.toDataURL(code, async (err, url) => {
-            if (err) {
-                console.error(err)
-                m.reply('Hubo un problema al generar el código QR. Por favor, intenta nuevamente más tarde.')
-                return
-            }
-
-            let caption = `Escanea este código QR para vincular:\nCódigo: ${code}`
-            await conn.sendMessage(m.chat, { url }, 'image', { caption, quoted: m })
-        })
-    } else {
-        await m.reply(`Opción no válida. Usa *${usedPrefix}${command} [codigo|qr]*`)
-    }
-}
-
-// Comando para validar el código y ser subbot
-let validateHandler = async (m, { conn, args }) => {
-    if (!args[0]) {
-        await m.reply('Por favor, proporciona un código de 8 dígitos para ser subbot.')
-        return
-    }
-
-    let code = args[0]
-
-    if (code.length !== 8 || !/^\d+$/.test(code)) {
-        await m.reply('El código debe ser un número de 8 dígitos.')
-        return
-    }
-
-    if (!subBotCodes[code]) {
-        await m.reply('Código inválido o ya utilizado.')
-        return
-    }
-
-    // Marcar el código como utilizado
-    subBotCodes[code] = false
-
-    // Añadir el usuario como subbot
-    let user = m.sender
-    if (!global.subBots) global.subBots = []
-    global.subBots.push(user)
-
-    await m.reply(`¡Felicidades! Ahora eres un subbot. 🎉`)
-}
-
-handler.help = ['vincular [codigo|qr]']
-handler.tags = ['tools']
-handler.command = ['vincular']
+import { sticker } from '../lib/sticker.js'
+import fetch from 'node-fetch'
+let MessageType = (await import('@whiskeysockets/baileys')).default
+let handler = async (m, { conn}) => {
+try {
+let who
+if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
+else who = m.chat
+if (!who) return m.reply(`🚩 Etiqueta o menciona a alguien`)
+if(m.quoted?.sender) m.mentionedJid.push(m.quoted.sender)
+if(!m.mentionedJid.length) m.mentionedJid.push(m.sender)
+let res = await fetch('https://nekos.life/api/v2/img/feed')
+let json = await res.json()
+let name = conn.getName(m.sender)
+let name2 = conn.getName(who)
+if (m.isGroup){
+let { url } = json
+let stiker = await sticker(null, url, `${name} está alimentando a ${name2}`)
+conn.sendFile(m.chat, stiker, null, { asSticker: true })}
+} catch (e) { }}
+handler.help = ['alimentar']
+handler.tags = ['fun']
+handler.command = ['alimentar', 'food']
 handler.group = true
 export default handler
-
-validateHandler.help = ['serSubBot']
-validateHandler.tags = ['tools']
-validateHandler.command = ['serSubBot']
-validateHandler.group = true
-export default validateHandler
