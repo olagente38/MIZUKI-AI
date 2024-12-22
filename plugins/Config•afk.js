6b25fe678b0147dc4a29a6a3d1db9e9c58aeb3b1
𝐒𝐏𝐄𝐂𝐓𝐑𝐔𝐌 𝐎𝐅𝐈𝐂𝐈𝐀𝐋 <cuentaoficialspectrum@gmail.com>
2024-12-22 10:08:57 -0500

    Update Config•afk.js

--- plugins/Config•afk.js
+++ plugins/Config•afk.js
@@ -1,7 +1,7 @@
 export function before(m) {
 const user = global.db.data.users[m.sender];
 if (user.afk > -1) {
-conn.reply(m.chat, `🚩 Dejastes De Estar Inactivo\n${user.afkReason ? 'Motivo De La Inactividad: ' + user.afkReason : ''}\n\n*Tiempo Inactivo: ${(new Date - user.afk).toTimeString()}*`, m, rcanal)
+conn.reply(m.chat, `★¸.•☆•.¸★ 𝐀𝐅𝐊 ★⡀.•☆•.★\n\n[🚨] 𝐇𝐚𝐬 𝐯𝐮𝐞𝐥𝐭𝐨 𝐚 𝐞𝐬𝐭𝐚𝐫 𝐚𝐜𝐭𝐢𝐯𝐨.\n${user.afkReason ? '[❓] 𝐑𝐚𝐳ó𝐧 𝐝𝐞 𝐥𝐚 𝐢𝐧𝐚𝐜𝐭𝐢𝐯𝐢𝐝𝐚𝐝: ' + user.afkReason : ''}\n[⏳] 𝐓𝐢𝐞𝐦𝐩𝐨 𝐈𝐧𝐚𝐜𝐭𝐢𝐯𝐨: ${(new Date - user.afk).toTimeString()}`, m, rcanal)
 user.afk = -1;
 user.afkReason = '';
 }
@@ -16,7 +16,7 @@ if (!afkTime || afkTime < 0) {
 continue;
 }
 const reason = user.afkReason || '';
-conn.reply(m.chat, `🚩 *El Usuario Esta Inactivo No Lo Etiquetes*`, m, rcanal)
+conn.reply(m.chat, `[🚨] 𝐄𝐥 𝐮𝐬𝐮𝐚𝐫𝐢𝐨 𝐞𝐬𝐭á 𝐢𝐧𝐚𝐜𝐭𝐢𝐯𝐨. 𝐍𝐨 𝐥𝐨 𝐦𝐞𝐧𝐜𝐢𝐨𝐧𝐞𝐬.`, m, rcanal)
 }
 return true;
 }
