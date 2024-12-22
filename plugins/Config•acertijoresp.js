4c9d46c59ae27d87e2b9608b78196a9e6fee8b35
𝐒𝐏𝐄𝐂𝐓𝐑𝐔𝐌 𝐎𝐅𝐈𝐂𝐈𝐀𝐋 <cuentaoficialspectrum@gmail.com>
2024-12-22 09:57:34 -0500

    Update Config•acertijoresp.js

--- plugins/Config•acertijoresp.js
+++ plugins/Config•acertijoresp.js
@@ -5,16 +5,16 @@ handler.before = async function(m) {
   const id = m.chat;
   if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^ⷮ/i.test(m.quoted.text)) return !0;
   this.tekateki = this.tekateki ? this.tekateki : {};
-  if (!(id in this.tekateki)) return m.reply('✨️ Ese acertijo ya ha terminado!');
+  if (!(id in this.tekateki)) return m.reply('Ese acertijo ya ha terminado!');
   if (m.quoted.id == this.tekateki[id][0].id) {
     const json = JSON.parse(JSON.stringify(this.tekateki[id][1]));
     if (m.text.toLowerCase() == json.response.toLowerCase().trim()) {
-      global.db.data.users[m.sender].estrellas += this.tekateki[id][2];
-      m.reply(`🌵 *Respuesta correcta!*\n+${this.tekateki[id][2]} Galletas`);
+      global.db.data.users[m.sender].exp += this.tekateki[id][2];
+      m.reply(`*Respuesta correcta!*\n+${this.tekateki[id][2]} Exp`);
       clearTimeout(this.tekateki[id][3]);
       delete this.tekateki[id];
-    } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply(`🚩 Casi lo logras!`);
-    else m.reply('🍭 Respuesta incorrecta!');
+    } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply(`Casi lo logras!`);
+    else m.reply('Respuesta incorrecta!');
   }
   return !0;
 };
