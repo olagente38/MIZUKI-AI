1c2d793ec02f6870f2c2769e92b3402e0c4001a6
olagente38 <benjaminmijahuanga@gmail.com>
2025-01-02 19:05:09 -0500

    Update Game•casino.js

--- plugins/Game•casino.js
+++ plugins/Game•casino.js
@@ -25,13 +25,13 @@ if (args.length < 1) return conn.reply(m.chat, '🚩 Ingresa la cantidad de ' + 
 if (user.chocolates >= count * 1) {
 user.chocolates -= count * 1
 if (Aku > Kamu) {
-conn.reply(m.chat, '`🍟 Veamos que numeros tienen!`\n\n'+ `➠ *YaemoriBot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *PERDISTE* ${formatNumber(count)} Chocolates 🍫.`.trim(), m, rcanal)
+conn.reply(m.chat, '`🍟 Veamos que numeros tienen!`\n\n'+ `➠ *Mizuki-Ai* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *PERDISTE* ${formatNumber(count)} Chocolates 🍫.`.trim(), m, rcanal)
 } else if (Aku < Kamu) {
 user.chocolates += count * 2
-conn.reply(m.chat, '`🍟 Veamos que numeros tienen!`\n\n'+ `➠ *YaemoriBot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *GANASTE* ${formatNumber(count * 2)} Chocolates 🍫.`.trim(), m, rcanal)
+conn.reply(m.chat, '`🍟 Veamos que numeros tienen!`\n\n'+ `➠ *Mizuki-Ai* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *GANASTE* ${formatNumber(count * 2)} Chocolates 🍫.`.trim(), m, rcanal)
 } else {
 user.chocolates += count * 1
-conn.reply(m.chat, '`🍟 Veamos que numeros tienen!`\n\n'+ `➠ *YaemoriBot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username} obtienes ${formatNumber(count * 1)} Chocolates 🍫.`.trim(), m, rcanal)}
+conn.reply(m.chat, '`🍟 Veamos que numeros tienen!`\n\n'+ `➠ *Mizuki-Ai* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username} obtienes ${formatNumber(count * 1)} Chocolates 🍫.`.trim(), m, rcanal)}
 } else conn.reply(m.chat, `No tienes *${formatNumber(count)} Chocolates 🍫* para apostar!`.trim(), m, rcanal)}
 
 handler.help = ['apostar *<cantidad>*']
