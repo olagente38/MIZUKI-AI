912b23e84a5e86375ecd692f222d9d70d93d1a30
olagente38 <benjaminmijahuanga@gmail.com>
2025-01-02 19:32:02 -0500

    Update Main•allfake.js

--- plugins/Main•allfake.js
+++ plugins/Main•allfake.js
@@ -109,9 +109,9 @@ global.fake = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo
 }}, { quoted: m }
 
 global.icono = [ 
-'https://qu.ax/eOTEH.jpeg',
-'https://qu.ax/eOTEH.jpeg',
-'https://qu.ax/eOTEH.jpeg'
+'https://qu.ax/NfnHz.jpeg',
+'https://qu.ax/NfnHz.jpeg',
+'https://qu.ax/NfnHz.jpeg'
 ].getRandom()
 
 global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: 100, newsletterName: channelRD.name, }, externalAdReply: { showAdAttribution: true, title: namebot, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icono, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, }, }}
